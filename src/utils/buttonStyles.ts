8af388c741debe7fbb9b282a7455a8ce05d98fbe
Gabriela Quintaes <gabrielapquintaes@gmail.com>
2026-03-18 09:42:37 -0300

    first commit

---
+++ src/utils/buttonStyles.ts
@@ -0,0 +1,5 @@
+export const defaultOutlineButtonClass =
+  "!border-[#23917e] !text-[#23917e] hover:!border-[#23917e] hover:!bg-[#23917e14]";
+
+export const redOutlineButtonClass =
+  "!border-[#ef4444] !text-[#ef4444] hover:!border-[#ef4444] hover:!bg-[#ef444414]";
